96b9d8222f9886ec976a6f28635aa7ec3496c363
munetsi <zunguziramunetsi@gmail.com>
2017-07-16 14:03:28 +0200

    initial commit

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 140}
+  validates :body, presence: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  mount Ckeditor::Engine => '/ckeditor'
+  root to: 'pages#home'
+
+  get '/about'=> 'pages#about'
+
+  get '/contact'=> 'pages#contact'
+
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
