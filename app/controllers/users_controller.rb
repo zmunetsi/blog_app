1fa68cab54964155c40fed1af904452d91ed135b
munetsi <zunguziramunetsi@gmail.com>
2017-07-23 13:42:13 +0200

    controller, commented action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
-  before_action :set_admin, only: [:dashboard]
+  #before_action :set_admin, only: [:dashboard]
   def dashboard
     @user = User.find(current_user.id)
     @posts = Post.all
