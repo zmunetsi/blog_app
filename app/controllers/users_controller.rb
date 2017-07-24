2e157d6ab457b6692f869703eae4c86f3ea95dff
munetsi <zunguziramunetsi@gmail.com>
2017-07-24 12:35:20 +0200

    admin delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
   before_action :set_admin, only: [:dashboard]
+  before_action :find_user, only: [:destroy]
 
   def dashboard
-    @user = User.find(current_user.id)
-    @user_posts = @user.posts.paginate(:page => params[:page], :per_page => 5)
+    @logged_user = User.find(current_user.id)
+    @user_posts = @logged_user.posts.paginate(:page => params[:page], :per_page => 5)
 
     @posts = Post.paginate(:page => params[:page], :per_page => 5)
     @users = User.all
+  end
+
+
+  def destroy
+    if @user.destroy
+      flash[:success] = "Successfully deleted user!"
+      redirect_to users_dashboard_path
+    else
+      flash[:danger] = "Error deleting user!"
+    end
   end
 
  private
@@ -16,5 +27,9 @@ class UsersController < ApplicationController
        current_user.update_attribute :admin, true
     end
 
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
